e99a403de7ccdcd49aa978da2ec20d574ba8fd43
astmain <1311192345@qq.com>
2025-08-26 22:09:01 +0800

    nextjs

--- nextjs5/app/page.tsx
+++ nextjs5/app/page.tsx
@@ -1,59 +1,21 @@
-"use client";
-import { Button, Modal, Form, Input, Checkbox } from "@arco-design/web-react";
-import { axios_api } from "./axios_api";
-import { useSnapshot, proxy } from "valtio";
-import Main from "@/pages/Main";
-import Flow from "@/pages/Flow";
+'use client'
+import { Button, Modal, Form, Input, Checkbox } from '@arco-design/web-react'
+import { axios_api } from './axios_api'
+import { useSnapshot, proxy } from 'valtio'
+import Main from '@/pages/Main'
 
-
-
+// 全局状态管理,管理用户信息和其他消息
 export const BUS = proxy({
   count: 0,
   user: {
-    name: "xupeng",
-    tel: "15160315110",
-    password: "123456",
+    name: 'xupeng',
+    tel: '15160315110',
+    password: '123456',
   },
-});
+})
 
 export default function Home() {
-  const snap = useSnapshot(BUS);
-  console.log(snap);
-  return (
-    <div className="flex flex-col gap-2">
-      <div>https://arco.design/react/components/button</div>
-      <div style={{ display: "none" }}>tel: {JSON.stringify(snap)}</div>
-
-      <Button
-        type="primary"
-        className="w-[120px]"
-        onClick={async () => (BUS.count += 1)}
-      >
-        add_count: {BUS.count}
-      </Button>
-
-      <div className="flex  gap-2">
-        <Button
-          type="primary"
-          onClick={async () => {
-            const res = await axios_api.get("/user");
-            console.log(res);
-          }}
-        >
-          get_user
-        </Button>
-        <Button
-          type="primary"
-          onClick={async () => {
-            const res = await axios_api.post("/user");
-            console.log(res);
-          }}
-        >
-          post_user
-        </Button>
-      </div>
-      <Main />
-      <Flow />
-    </div>
-  );
+  const snap = useSnapshot(BUS)
+  console.log('snap', snap)
+  return <div>user: {JSON.stringify(BUS.user)}</div>
 }
--- nextjs5/pages/Main.tsx
+++ nextjs5/pages/Main.tsx
@@ -4,17 +4,7 @@ import { BUS } from '@/app/page'
 export default function Main() {
   return (
     <div>
-      <div>
-        <Button type="primary" onClick={async () => (BUS.count += 1)}>
-          main-add_count: {BUS.count}
-        </Button>
-      </div>
-
-
-
-      <div>
-
-      </div>
+ 
     </div>
   )
 }
